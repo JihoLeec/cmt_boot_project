ed03e4fcafe5dfd070402979961e5a90ee78c5d7
JihoLeec <JihoLee.py@gmail.com>
2024-07-24 13:32:45 +0900

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   accounts/__pycache__/urls.cpython-312.pyc
    	modified:   accounts/urls.py
    	modified:   db.sqlite3
    	modified:   eco/__pycache__/settings.cpython-312.pyc
    	modified:   mysite/__pycache__/views.cpython-312.pyc
    	modified:   mysite/views.py

--- mysite/views.py
+++ mysite/views.py
@@ -7,8 +7,8 @@ def index(request):
     return render(request, 'mysite/content_list.html', context)
 
 def detail(request, content_id):
- content_list = get_object_or_404(MainContent, pk=content_id)
- context = { 'content_list': content_list}
- return render(request, 'mysite/content_detail.html', context)
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = { 'content_list': content_list}
+    return render(request, 'mysite/content_detail.html', context)
 
 # Create your views here.
